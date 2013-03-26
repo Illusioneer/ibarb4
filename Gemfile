7ae5632d4dcaccdafd51475d34eaf35696d5ad7b
Illusioneer <khillton@gmail.com>
2013-03-26 12:35:03 -0400

    Improved user model

--- Gemfile
+++ Gemfile
@@ -7,6 +7,12 @@ gem 'rails', '4.0.0.beta1'
 gem 'pg'
 gem 'quiet_assets'
 gem 'googlevoiceapi'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-identity'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  
+  
+  %w[provider provider_uid provider_secret].each do |key|
+    attr_accessible key
+    scope "has_#{key}", lambda { |value| where("metadata @> (? => ?)", key, value) }
+    
+    define_method(key) do
+      metadata && metadata[key]
+    end
+  
+    define_method("#{key}=") do |value|
+      self.metadata = (metadata || {}).merge(key => value)
+    end
+  end  
+
 end
