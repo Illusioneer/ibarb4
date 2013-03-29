28ee821f770edbfbf42f337a0155e425f57bb320
Illusioneer <khillton@gmail.com>
2013-03-29 15:29:58 -0400

    Added backendex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  
+  attr_accessible :uid, :name, :password, :mail, :tagline, :last_update, :last_access, :status, :timezone, :metadata, :user_roles
   
   %w[provider provider_uid provider_secret].each do |key|
     attr_accessible key
@@ -15,3 +15,6 @@ class User < ActiveRecord::Base
   end  
 
 end
+
+
+      
